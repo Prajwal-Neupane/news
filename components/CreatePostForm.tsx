5e5c7cf8d98383a7c9f263d1ff0cb7f622b45e79
Prajwal Neupane <prajwalneupane.981@gmail.com>
2024-01-13 18:22:20 +0545

    createpost functionalty completed

--- components/CreatePostForm.tsx
+++ components/CreatePostForm.tsx
@@ -1,8 +1,28 @@
 "use client";
+import Link from "next/link";
 import React, { useState } from "react";
 import { IoMdAdd } from "react-icons/io";
+import { RiLinksFill } from "react-icons/ri";
+import { MdDelete } from "react-icons/md";
 
 const CreatePostForm = () => {
+  const [links, setLinks] = useState<string[]>([]);
+  const [linkInput, setLinkInput] = useState("");
+  const handleLink = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    if (linkInput.trim() !== "") {
+      setLinks([...links, linkInput]);
+      setLinkInput("");
+    }
+  };
+  const handleLinkChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setLinkInput(e.target.value);
+  };
+  const handleLinkDelete = (id: number) => {
+    const updatedLinks = [...links];
+    updatedLinks.splice(id, 1);
+    setLinks(updatedLinks);
+  };
   const categoriesList = [
     {
       id: 1,
@@ -45,13 +65,42 @@ const CreatePostForm = () => {
           placeholder="Content"
           required
         />
+        <div className="flex flex-col gap-0">
+          {links && links.length > 0
+            ? links.map((link, index) => (
+                <div
+                  className="flex items-center gap-1 mt-3 md:text-xl "
+                  key={index}
+                >
+                  <RiLinksFill className="text-dark font-semibold hover:underline hover:cursor-pointer hover:scale-y-105 transition-all " />{" "}
+                  <Link
+                    href={link}
+                    className="text-dark font-semibold hover:underline hover:cursor-pointer hover:scale-y-105 transition-all max-w-full overflow-hidden text-ellipsis "
+                  >
+                    {link}
+                  </Link>
+                  <MdDelete
+                    onClick={() => handleLinkDelete(index)}
+                    color="red "
+                    size={25}
+                    className="text-dark text-right font-semibold hover:underline hover:cursor-pointer hover:scale-y-105 transition-all  ml-6"
+                  />
+                </div>
+              ))
+            : ""}
+        </div>
         <div className="w-full flex gap-6 ">
           <input
+            onChange={handleLinkChange}
+            value={linkInput}
             type="text"
             placeholder="Links"
             className="w-full px-3 py-4 outline-none rounded-md text-xl border border-slate-400"
           />
-          <button className="font-semibold rounded-md bg-[#365486] justify-center text-white text-xl flex gap-3 items-center px-2 py-2 w-[20%]">
+          <button
+            onClick={handleLink}
+            className="font-semibold rounded-md bg-[#365486] justify-center text-white text-xl flex gap-3 items-center px-2 py-2 w-[20%]"
+          >
             <IoMdAdd size={30} /> Add
           </button>
         </div>
