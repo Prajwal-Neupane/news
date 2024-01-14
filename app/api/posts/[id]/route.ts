ac439e5fa0af4c072ed01edce06761a192975aaf
Prajwal Neupane <prajwalneupane.981@gmail.com>
2024-01-14 20:20:29 +0545

    update post completed

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -17,3 +17,34 @@ export const GET = async (
     );
   }
 };
+
+export const PUT = async (
+  req: Request,
+  { params }: { params: { id: string } }
+) => {
+  try {
+    const { title, content, links, categories, imageUrl, publicId } =
+      await req.json();
+    if (!title || !content) {
+      return NextResponse.json(
+        { message: "Fields are required" },
+        { status: 201 }
+      );
+    }
+    const updatePost = await prisma.post.update({
+      data: {
+        title,
+        content,
+        links,
+        catName: categories,
+        imageUrl,
+        publicId,
+      },
+      where: { id: params.id },
+    });
+    return NextResponse.json(updatePost);
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "Failed to update post" });
+  }
+};
