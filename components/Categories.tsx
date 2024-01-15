b3a9b7ab83ce9534cb7e8ff428f15b3330582956
Prajwal Neupane <prajwalneupane.981@gmail.com>
2024-01-15 12:08:16 +0545

    categories added from backend

--- components/Categories.tsx
+++ components/Categories.tsx
@@ -1,36 +1,55 @@
 import Link from "next/link";
 import React from "react";
 
-const Categories = () => {
-  const categoriesList = [
-    {
-      id: 1,
-      name: "technology",
-    },
-    {
-      id: 2,
-      name: "travel",
-    },
-    {
-      id: 3,
-      name: "foods",
-    },
-    {
-      id: 4,
-      name: "entertainment",
-    },
-  ];
+interface Category {
+  id: string;
+  catName: string;
+}
+
+const getCategories = async () => {
+  try {
+    const res = await fetch(`${process.env.NEXTAUTH_URL}/api/categories`);
+    if (res.ok) {
+      const categories = await res.json();
+      // console.log(categories);
+      return categories;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const Categories = async () => {
+  const categories = await getCategories();
+  // const categoriesList = [
+  //   {
+  //     id: 1,
+  //     name: "technology",
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "travel",
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "foods",
+  //   },
+  //   {
+  //     id: 4,
+  //     name: "entertainment",
+  //   },
+  // ];
   return (
     // flex gap-7 flex-wrap
-    <div className="grid  lg:grid-cols-4 md:grid-cols-3 grid-cols-2 gap-8">
-      {categoriesList.map((category) => {
+    <div className="grid  lg:grid-cols-4 md:grid-cols-3 grid-cols-2  gap-8">
+      {categories.map((category: Category) => {
         return (
           <Link
-            className="btn md:text-xl"
-            href={`/categories/${category.name}`}
+            className="btn md:text-xl "
+            href={`/categories/${category.catName}`}
             key={category.id}
           >
-            {category.name.charAt(0).toUpperCase() + category.name.slice(1)}
+            {category.catName}
           </Link>
         );
       })}
