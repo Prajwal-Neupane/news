97f3136b1da122aa5207cab8bf357fbe2b236ee2
Prajwal Neupane <prajwalneupane.981@gmail.com>
2024-01-17 11:26:30 +0545

    updated toast and refreshRouter

--- components/DeleteButton.tsx
+++ components/DeleteButton.tsx
@@ -1,37 +1,40 @@
 "use client";
 import React from "react";
 import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 interface DeleteProps {
   postId: string;
 }
 
-const handleDelete = async (postId: string) => {
-  const deleteImage = async (publicId: string) => {
-    const res = await fetch("/api/removeImage", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ publicId }),
-    });
-  };
-  // console.log("NEXTAUTH_URL:", process.env.NEXTAUTH_URL);
-  // const apiEndpoint = `${process.env.NEXTAUTH_URL}/api/posts/${postId}`;
-  // console.log("API Endpoint:", apiEndpoint);
-
-  const response = await fetch(`/api/posts/${postId}`, {
-    cache: "reload",
-    method: "DELETE",
-  });
-  if (response.ok) {
-    const post = await response.json();
-    const { publicId } = post;
-    await deleteImage(publicId);
-    toast.success("Post deleted successfully");
-  }
-};
-
 const DeleteButton = ({ postId }: DeleteProps) => {
+  const router = useRouter();
+  const handleDelete = async (postId: string) => {
+    const deleteImage = async (publicId: string) => {
+      const res = await fetch("/api/removeImage", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ publicId }),
+      });
+    };
+    // console.log("NEXTAUTH_URL:", process.env.NEXTAUTH_URL);
+    // const apiEndpoint = `${process.env.NEXTAUTH_URL}/api/posts/${postId}`;
+    // console.log("API Endpoint:", apiEndpoint);
+
+    const response = await fetch(`/api/posts/${postId}`, {
+      cache: "reload",
+      method: "DELETE",
+    });
+    if (response.ok) {
+      const post = await response.json();
+      const { publicId } = post;
+      await deleteImage(publicId);
+      toast.success("Post deleted successfully");
+      router.refresh();
+    }
+  };
+
   return (
     <button
       onClick={() => handleDelete(postId)}
